52ea8209508964c21cdaf12ec21ef04f165e8467
Ben64 <ben64@ben64.com>
2024-10-19 17:53:00 -0700

    make Lucretia playable in the adoption scene

---
+++ project/characters/npcs/cat_owner/CatOwner.cs
@@ -0,0 +1,31 @@
+using Godot;
+using Godot.Collections;
+using System;
+
+public partial class CatOwner : CharacterBody2D
+{
+	[Export] public float Speed = 80.0f;
+
+	Vector2 PlayerInput;
+	Vector2 PlayerMovement;
+
+	public override void _Ready()
+	{
+		//GlobalPosition = Globals.Instance.Spawn;
+		//Globals.Instance.Spawn = Vector2.Zero;
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Move();
+	}
+
+	public void Move()
+	{
+		PlayerInput = Input.GetVector("move_left", "move_right", "move_up", "move_down").Normalized();
+		PlayerMovement = PlayerInput * Speed;
+		Velocity = PlayerMovement;
+		MoveAndSlide();
+	}
+
+}
